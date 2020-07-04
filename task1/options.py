230d000ec330003fd57678f59b8070cc46269a64
= <araariadem0@gmail.com>
2020-07-04 14:12:48 +0100

    decrease all pending tasks count after one complete

--- task1/options.py
+++ task1/options.py
@@ -137,6 +137,17 @@ class WebScraper:
         return page_content, None, True
 
 
+def decrease_all_pending_tasks_count():
+    try:
+        for tbl in tbl_page_data.objects.all():
+            try:
+                tbl.pending_task = tbl.pending_task - 1
+                tbl.save()
+            except:
+                pass
+    except:
+        pass
+
 def get_pending_count():
     return tbl_page_data.objects.filter(status_process=tbl_page_data.PROCESSING_STATUS).count()
 
@@ -193,6 +204,7 @@ def _start_task(tbl):
 
     finally:
         tbl.save()
+        decrease_all_pending_tasks_count()
 
 
 def start_task(tbl):
