4eb6951d29fa5e3d776f04e3d7dfb9a8e630aacf
= <araariadem0@gmail.com>
2020-07-06 10:28:57 +0100

    add repr e

--- task2/options.py
+++ task2/options.py
@@ -262,7 +262,7 @@ def _start_crawl_task(tbl):
             tbl.status_code = status_code
         except Exception as e:
             tbl.status_process = tbl_crawl_task.ERROR_STATUS
-            tbl.error_msg = "Server memory is Full, server cannot crawl more urls"
+            tbl.error_msg = repr(e) # "Server memory is Full, server cannot crawl more urls"
             tbl.status_code = status_code
 
         finally:
