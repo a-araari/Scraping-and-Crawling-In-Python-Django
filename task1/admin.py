4aa6ef1cc014f5f0854d486c417c8f7c2986eab3
= <araariadem0@gmail.com>
2020-07-06 17:14:42 +0100

    FIX pending_task for scrape2

--- task1/admin.py
+++ task1/admin.py
@@ -5,4 +5,4 @@ from .models import tbl_page_data
 
 @admin.register(tbl_page_data)
 class tbl_page_dataAdmin(admin.ModelAdmin):
-    list_display = ('task_id', 'status_code', 'status_process', 'error_msg')
+    list_display = ('task_id', 'url', 'status_process', 'pending_task')
