eb448498c5b66e382063a3ede70d1207612a7635
= <araariadem0@gmail.com>
2020-07-06 12:39:16 +0100

    add loggeradmin

--- task2/admin.py
+++ task2/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import tbl_crawl_task, tbl_crawl_task_data
+from .models import tbl_crawl_task, tbl_crawl_task_data, Logger
 
 
 @admin.register(tbl_crawl_task)
@@ -11,3 +11,8 @@ class tbl_crawl_taskAdmin(admin.ModelAdmin):
 @admin.register(tbl_crawl_task_data)
 class tbl_crawl_taskAdmin(admin.ModelAdmin):
     list_display = ('task_id', 'url', 'depth_level')
+
+
+@admin.register(Logger)
+class LoggertaskAdmin(admin.ModelAdmin):
+    list_display = ('text')
