8d602903076c194ce9a28743d77ef7c1c8042cd9
= <araariadem0@gmail.com>
2020-07-08 17:03:28 +0100

    remove log to __init__

--- task1/__init__.py
+++ task1/__init__.py
@@ -1,22 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from task2.models import Logger
 
 
 driver = None
-count = 0
-
-
-def log(*text):
-    l, created = Logger.objects.get_or_create(id=1)
-    l.text = l.text + '\n' + str(text)
-    l.save()
 
 
 def get_driver():
-    global driver, count
-    
-    log("Getting instance", count)
+    global driver
 
     if driver is None:
 
@@ -25,7 +15,5 @@ def get_driver():
         options.add_argument('--no-sandbox')
 
         driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
-        count += 1
-        log("new instance", count)
 
     return driver
