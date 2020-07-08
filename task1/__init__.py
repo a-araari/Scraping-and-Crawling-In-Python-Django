094f325f5943cc1c2583e0d22d9e2b233315d334
= <araariadem0@gmail.com>
2020-07-08 16:56:20 +0100

    add log to __init__

--- task1/__init__.py
+++ task1/__init__.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from task2.models import Logger
 
 
 driver = None
+count = 0
+
+
+def log(*text):
+    l, created = Logger.objects.get_or_create(id=1)
+    l.text = l.text + '\n' + str(text)
+    l.save()
 
 
 def get_driver():
-    global driver
+    global driver, count
+    
+    log("Getting instance", count)
 
     if driver is None:
 
@@ -15,5 +25,7 @@ def get_driver():
         options.add_argument('--no-sandbox')
 
         driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+        count += 1
+        log("new instance", count)
 
     return driver
