47dde8f018afc61e3456c363720c1b2d33cb1506
= <araariadem0@gmail.com>
2020-07-11 20:07:21 +0100

    change static to static_files

--- pro/settings.py
+++ pro/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'static_files'
 
 
 REST_FRAMEWORK = {
