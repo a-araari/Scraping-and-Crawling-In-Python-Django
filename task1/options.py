1bcec46fe7cab2e0b0e3e9699a727ad65c7d2c86
= <araariadem0@gmail.com>
2020-07-13 02:20:24 +0100

    Debug25

--- task1/options.py
+++ task1/options.py
@@ -136,8 +136,6 @@ def decrease(pt):
             try:
                 if t.pending_task == 0:
                     continue
-                print('decreasing', t.pending_task)
-                time.sleep(float(f'{random.randint(0, 2)}.{random.randint(0, 99999)}'))
                 t = tbl_page_data.objects.get(task_id=t.task_id)
                 
                 if t.pending_task == 0:
