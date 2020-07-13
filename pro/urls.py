fe68f7ca471969f1789e0c026bfb31eb26d4a9f6
= <araariadem0@gmail.com>
2020-07-13 12:06:00 +0100

    Debug1

--- pro/urls.py
+++ pro/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from task1.models import tbl_page_data
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
